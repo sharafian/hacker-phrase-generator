fd6a9d42af475fa68c7c32be044964889ca7ebe9
harryprotist <benmS@gmail.com>
2014-07-30 15:51:16 -0700

    fixed tabbing

--- runhackergen.py
+++ runhackergen.py
@@ -2,9 +2,9 @@
 from hackergen import phrasegen
 
 def getPhrase(*args, **kwargs):
-    return phrasegen.getPhrase(*args, **kwargs)
+	return phrasegen.getPhrase(*args, **kwargs)
 
 def printPhrase(*args, **kwargs):
-    print "Generated Phrase:\n\n"+getPhrase(*args, **kwargs)
+	print "Generated Phrase:\n\n"+getPhrase(*args, **kwargs)
 
 printPhrase()
